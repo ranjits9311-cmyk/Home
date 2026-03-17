af57d70bdc58ca6fffb88f4674d105967e09962d
ranjits9311-cmyk <ranjits9311@gmail.com>
2026-03-17 13:57:04 -0400

    ready

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('theme-toggle');
+
+toggle.addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', newTheme);
+});
